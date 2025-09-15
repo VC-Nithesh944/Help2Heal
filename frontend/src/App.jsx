80e59b564102b08c28986abd962ff17e86cab68a
Meghanan-21 <meghanan515@gmail.com>
2025-09-15 18:44:39 +0530

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,12 @@ import MyAppointments from "./pages/MyAppointments";
 import Appointment from "./pages/Appointment";
 import Contact from "./pages/Contact";
 import Footer from "./components/footer";
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
